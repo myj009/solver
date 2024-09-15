b75a92a2df689979c849145e13ba640bf6a62c73
myj009 <mohammad.jagora@deliveroo.com>
2024-09-15 19:05:29 +0530

    Chat-WIP

---
+++ app/chat/page.tsx
@@ -0,0 +1,28 @@
+import { ChatMessageList } from "@/components/ui/chat/chat-message-list";
+import {
+  ChatBubble,
+  ChatBubbleAvatar,
+  ChatBubbleMessage,
+} from "@/components/ui/chat/chat-bubble";
+import { ChatInput } from "@/components/ui/chat/chat-input";
+import { Button } from "@/components/ui/button";
+import { CornerDownLeft } from "lucide-react";
+
+export default function ChatPage() {
+  return (
+    <>
+      <ChatMessageList>
+        <ChatBubble>
+          <ChatBubbleAvatar />
+          <ChatBubbleMessage>Message and other content here</ChatBubbleMessage>
+        </ChatBubble>
+      </ChatMessageList>
+      <div className="flex-1" />
+      <ChatInput placeholder="Type your message here..." />
+      <Button size="sm" className="ml-auto gap-1.5">
+        Send Message
+        <CornerDownLeft className="size-3.5" />
+      </Button>
+    </>
+  );
+}
