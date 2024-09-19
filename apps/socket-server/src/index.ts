da284a3848e7205e8e5b4b375075468cb0dcad2c
myj009 <mohammad.jagora@deliveroo.com>
2024-09-19 09:50:53 +0530

    Adding socker server

---
+++ apps/socket-server/src/index.ts
@@ -0,0 +1,22 @@
+import express from "express";
+import http from "http";
+import dotenv from "dotenv";
+import { Server } from "socket.io";
+
+dotenv.config();
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+app.get("/", (req, res) => {
+  return res.send("Hellow");
+});
+
+io.on("connection", (socket) => {
+  console.log("user connected");
+});
+
+server.listen(process.env.PORT || 8000, () => {
+  console.log(`Listening on port ${process.env.PORT}`);
+});
